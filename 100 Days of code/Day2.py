061a3a1d7ca4b81539694a36540c836a74c68f3a
Shabega <112605561+Shabega@users.noreply.github.com>
2023-07-18 21:53:48 +0530

    Add files via upload

---
+++ 100 Days of code/Day2.py
@@ -0,0 +1,11 @@
+print("Welcome to the tip calculator.")
+bill =float(input("What was the total bill? $"))
+tip=int(input("How much tip would you like to give?10,12,or 15?"))
+people=int(input("How many people to split the bill"))
+tip_as_percent=tip/100
+total_tip_amount=bill*tip_as_percent
+total_bill=bill+total_tip_amount
+person_percent=total_bill/people
+final_amount=round(person_percent,2)
+final_amt="{:.4f}".format(person_percent)
+print(f"Each person should pay${final_amt}")
